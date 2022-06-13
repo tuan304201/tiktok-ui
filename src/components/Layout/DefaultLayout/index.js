7bea2146550efa408987889101ca8e3a73581c14
TuanAnh <tuananh30042001@gmail.com>
2022-06-13 14:47:46 +0700

    scss header,layout

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,19 @@
-import Header from './Header';
+import classNames from 'classnames/bind';
+import Header from '~/components/Layout/components/Header';
 import Slidebar from './Slidebar';
+import styles from './DefaultLayout.module.scss';
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
-        <div>
-            <Header />
-            <div className="container">
-                <Slidebar />
-                <div className="content">{children}</div>
+        <div className={cx('wrapper')}>
+            <div>
+                <Header />
+                <div className={cx('container')}>
+                    <Slidebar />
+                    <div className={cx('content')}>{children}</div>
+                </div>
             </div>
         </div>
     );
